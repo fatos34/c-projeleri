e95fe18a96cf85d40afd6567ada214c20ef58cff
Fatımagül Karakaş <fatiimaguull.krks24@gmail.com>
2025-07-03 23:11:58 +0300

    Create 09_mantıksal_operatörler.cpp

---
+++ 09_mantıksal_operatörler.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+int a=34,b=45,c=45;
+printf("%d",(44>33)&&(56==56));
+printf("\n%d",(33>55)&&(a<=b));
+printf("\n%d",(b==c)&&(a>b));
+printf("\n%d",(a<=b)&&(45==c));
+}
